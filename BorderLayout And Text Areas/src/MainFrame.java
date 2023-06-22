f462c543e9e3558b2da8b02b8f0149481c5e1826
Rishav parasar <rishavparasar2004@gmail.com>
2023-06-22 15:38:02 +0530

    new Mainframe

--- BorderLayout And Text Areas/src/MainFrame.java
+++ BorderLayout And Text Areas/src/MainFrame.java
@@ -5,7 +5,8 @@ import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
     private TextPanel textpanel;
-    private JButton b1;
+//    private JButton b1;
+    private Toolbar toolbar;
     public MainFrame()
     {
         super("Hello World");
@@ -14,17 +15,18 @@ public class MainFrame extends JFrame {
         setVisible(true);
         setLayout(new BorderLayout());//using the BorderLayout layout manager
 
-//        textarea = new TextArea();//creating instances of the private declarations;
-        b1 = new JButton("Click me vro!");
         textpanel = new TextPanel();
-        add(textpanel,BorderLayout.CENTER);
-        add(b1,BorderLayout.SOUTH);//added locations
+        toolbar = new Toolbar();
 
-        //creating the action listener interface for the button
-        b1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                textpanel.appendText("Hello Parasar\n");
+        toolbar.setStringListener(new StringListener() {
+            public void textEmmited(String text) {
+                textpanel.appendText(text);
             }
         });
+
+        add(textpanel,BorderLayout.CENTER);
+        add(toolbar,BorderLayout.NORTH);
+
+
     }
 }
